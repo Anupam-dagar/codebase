191ed9db2d285a71a7d07352381e93d48053b0fd
Anupam-dagar <dagaranupam@gmail.com>
2017-04-09 17:05:43 +0530

    Added stack using top as pointer

---
+++ Hackerearth/basic programming/printhackerearth.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+int main()
+{
+	long int i , N , value = 0;
+	int h = 0 , a = 0 , c = 0, k = 0, e = 0 , r = 0 , t =0 ;
+	char str[1000000], ch[7] = {0}; 
+
+	char r1;
+	scanf("%d",&N);
+
+	scanf("%s",str);
+
+	if(N <11) // single hcakerearth will always contain atleast 11 characters 
+	{
+		printf("%d", value );
+
+	}
+
+	else
+	{
+		for(i = 0 ; i < N ; i++) // h a c k e r e a r t h --- h a c k e r t 
+		{
+
+			r1 = str[i];
+			//	printf("hrlloo" );
+			//	    switch(r1)
+			//	        {
+			//	            case 'h':
+			//	                h++;
+			//	                    break;
+			//	                         
+			//	                             case 'a':
+			//	                                 a++;
+			//	                                     break;
+			//	                                          
+			//	                                              case 'c':
+			//	                                                  c++;
+			//	                                                      break;
+			//	                                                           
+			//	                                                               case 'k':
+			//	                                                                   k++;
+			//	                                                                       break;
+			//	                                                                            
+			//	                                                                                case 'e':
+			//	                                                                                    e++;
+			//	                                                                                        break;
+			//	                                                                                             
+			//	                                                                                                 case 'r':
+			//	                                                                                                     r++;
+			//	                                                                                                         break;
+			//	                                                                                                              
+			//	                                                                                                                  case 't':
+			//	                                                                                                                      t++;
+			//	                                                                                                                          break;
+			//	                                                                                                                               
+			//	                                                                                                                                   default:
+			//	                                                                                                                                        
+			//	                                                                                                                                            break;
+			//	                                                                                                                                                 
+			//	                                                                                                                                                     }
+			//	                                                                                                                                                         }
+			//	                                                                                                                                                             //h a c k e r t
+			//	                                                                                                                                                                  
+			//	                                                                                                                                                                      //	printf("%d %d %d %d %d %d %d", h , a, c , k , e, r, t );
+			//	                                                                                                                                                                          while(h>=2 && a>=2 && c>=1 && k>=1 && e>=2 &&r>=2&&t>=1) 
+			//	                                                                                                                                                                              {
+			//	                                                                                                                                                                                  h-=2; // keep subtracting till it condition become false
+			//	                                                                                                                                                                                      a-=2; // and at the end if whole subtract then it will be counted as one 
+			//	                                                                                                                                                                                          c-=1; // and so on 
+			//	                                                                                                                                                                                              k-=1;
+			//	                                                                                                                                                                                                  e-=2;
+			//	                                                                                                                                                                                                      r-=2;
+			//	                                                                                                                                                                                                          t-=1;
+			//	                                                                                                                                                                                                              value++;
+			//	                                                                                                                                                                                                                  }
+			//	                                                                                                                                                                                                                      printf("%d",value );
+			//	                                                                                                                                                                                                                          return 0;
+			//	                                                                                                                                                                                                                              }
+			//	                                                                                                                                                                                                                                  }
+			//	                                                                                                                                                                              }
+			//	        }
+			}
+}
+}
---
+++ data structure/stackarrpointer.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+int size = 10;
+int items[100]; 
+int empty(int *top)
+{
+	if(*top == -1)
+	{
+		return 1;
+	}
+	else 
+		return 0;
+}
+int full(int *top)
+{
+	if(*top == size -1)
+	{
+		return 1;
+	}
+	else 
+		return 0;
+}
+int push(int items[],int i,int *top)
+{
+	if(full(top))
+	{
+	
+	}
+	else 
+	{
+		*top=*top+1;
+		items[*top] = i;
+		return 1;
+	}
+}
+int pop(int items[],int *top)
+{
+	if(empty(top))
+	{
+	}
+	else
+	{
+		*top=*top-1;;
+		return 1;
+	}
+}
+int stacktop(int *top)
+{
+	printf("%d\n",items[*top]);
+}
+int main()
+{
+	int top = -1,i,choice,element;
+	printf("Array implementation of stack.\nSize of stack is 10\n\n");
+	while(i)
+	{
+		printf("Choose an option:\n");
+		printf("1.Push\n2.Pop\n3.Top Element\n4.Stack Status\n5.Print Stack\n6.Exit\n");
+		scanf("%d",&choice);
+		printf("\n");
+		switch(choice)
+		{
+			case 1:
+				if(!full(&top))
+				{
+					printf("Enter element to push:\n");
+					scanf("%d",&element);
+					push(items,element,&top);
+				}
+				else if(full(&top))
+				{
+					printf("overflow\n");
+				}
+				printf("\n");
+				break;
+			case 2:	if(!empty(&top))
+				{
+					pop(items,&top);
+					printf("Element popped\n");
+				}
+				else if(empty(&top))
+				{
+					printf("underflow\n");
+				}
+				printf("\n");
+				break;
+			case 3:	printf("Top element is %d\n",stacktop(&top));
+				printf("\n");
+				break;
+			case 4: if(empty(&top))
+				{}
+				else if(full(&top))
+				{}
+				else
+					printf("stack is partially filled\n");
+				printf("\n");
+				break;
+			case 5: if(!empty(&top))
+				{
+					printf("stack is ");
+					for(i=0;i<=top;i++)
+					{
+						printf("%d ",items[i]);
+					}
+					printf("\n");
+				}
+				else if(empty(&top))
+				{
+					printf("stack is empty\n");
+				}
+				printf("\n");
+				break;
+			case 6: printf("Exiting....\n");
+				break;	
+			default : printf("invalid input\n");	
+		}
+		if(choice == 6)
+		{
+			break;
+		}
+	}
+	return 0;
+}  
+
