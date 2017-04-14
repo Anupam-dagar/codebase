403644c60659da8e557aae9b9c4785e11f1b0713
Anupam-dagar <dagaranupam@gmail.com>
2017-04-14 22:30:47 +0530

    Added main function.

--- Others/calculator.cpp
+++ Others/calculator.cpp
@@ -67,5 +67,77 @@ int main()
 	long long int num1 = 100;
 	long long int num2 = 5;
 	long long int answer;
-	long long int ans;
+	double ans;
+	int input;
+	cout << "Welcome to Large Values Calculator\n";
+	cout << "Please note for large number multiplications,ensure that answer is within 10^18 else it may produce wrong result.\n";
+	while(1)
+	{
+		cout << "Select a valid option:\n";
+		cout << "1.Addition.\n2.Subtraction.\n3.Multiplication.\n4.Division.\n5.Greatest Common Divisor(GCD).\n6.Least Common Multiple(LCM).\n7.Factorial.\n8.Exit.\n";
+		cin >> input;
+		switch(input)
+		{
+			case 1: cout << "Enter number one:\n";
+				cin >> num1;
+				cout << "Enter number two:\n";
+				cin >> num2;
+				cout << endl;
+				answer = add(&num1,&num2);
+				cout << answer << endl << endl;
+				break;
+			case 2: cout << "Enter number one:\n";
+				cin >> num1;
+				cout << "Enter number two:\n";
+				cin >> num2;
+				cout << endl;
+				answer = subtract(&num1,&num2);
+				cout << answer << endl << endl;
+				break;
+			case 3: cout << "Enter number one:\n";
+				cin >> num1;
+				cout << "Enter number two:\n";
+				cin >> num2;
+				cout << endl;
+				answer = multiply(&num1,&num2);
+				cout << answer << endl << endl;
+				break;
+			case 4: cout << "Enter number one:\n";
+				cin >> num1;
+				cout << "Enter number two:\n";
+				cin >> num2;
+				cout << endl;
+				ans = divide(&num1,&num2);
+				cout << ans << endl << endl;
+				break;
+			case 5: cout << "Enter number one:\n";
+				cin >> num1;
+				cout << "Enter number two:\n";
+				cin >> num2;
+				cout << endl;
+				answer = GCD(&num1,&num2);
+				cout << answer << endl << endl;
+				break;
+			case 6: cout << "Enter number one:\n";
+				cin >> num1;
+				cout << "Enter number two:\n";
+				cin >> num2;
+				cout << endl;
+				answer = lcm(&num1,&num2);
+				cout << answer << endl << endl;	
+			case 7: cout << "Enter number\n";
+				cin >> num1;
+				cout << endl;
+				answer = factorial(&num1);
+				cout << answer << endl << endl;
+				break;
+			case 8: cout << "Exiting...\n";
+				break;
+			default:cout << "Enter a valid input.\n";
+		}
+		if(input == 8)
+		{
+			break;
+		}
+	}
 }
